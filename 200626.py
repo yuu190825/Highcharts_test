ec1a5122b1420609a9c3aafd49de162558b3c183
yuu190825 <56215608+yuu190825@users.noreply.github.com>
2020-06-27 16:14:12 +0800

    Python 題目實做 2 updata

--- 200626.py
+++ 200626.py
@@ -1,21 +1,22 @@
-f = open('200626.txt')
-i = []
-for line in f:
-    i.append(line)
-for a in range(len(i)):
-    i[a] = i[a].rstrip('\n')
-n = int(i[0])
-x = 0
-y = 0
-score = []
-for a in range(1, n + 1):
-    for b in i[a]:
-        if b == 'O':
-            x += (1 + y)
-            y += 1
-        else:
-            y = 0
-    score.append(x)
+for a in range(2):
+    f = open(f'./in{str(a+1)}.txt')
+    i = []
+    for line in f:
+        i.append(line)
+    for a in range(len(i)):
+        i[a] = i[a].rstrip('\n')
+    n = int(i[0])
     x = 0
     y = 0
-print(score) # 10, 9, 6, 55, 20
+    score = []
+    for a in range(1, n + 1):
+        for b in i[a]:
+            if b == 'O':
+                x += (1 + y)
+                y += 1
+            else:
+                y = 0
+        score.append(x)
+        x = 0
+        y = 0
+    print(score) # 10, 9, 6, 55, 20 and 198, 108, 24, 31, 61, 1, 0
